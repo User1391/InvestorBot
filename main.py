f802823ea9c64916db740a1533db667433bdab04
Max Penders <max.penders@gmail.com>
2020-11-28 23:09:55 -0500

    What we've done so far

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from alpha_vantage.timeseries import TimeSeries
+import ta
 
-def predictStockDirection:
-    
+def getData(ticker):
+    ts = TimeSeries(key='UFQL6BJD00VWBLZB',output_format='pandas', indexing_type='integer')
+    data, metadata = ts.get_daily(ticker)
+    # Remember, ^^data^^ is now a Pandas series with our daily data
+    highs = data['2. high']
+    length = int(highs.size)
+    lows = data['3. low']
+    closes = data['4. close']
+    return highs, lows, closes, length
+
+def indicationADX(ticker):
+    high, low, close, len = getData(ticker)
+    indicator = ta.trend.ADXIndicator(high, low, close, len)
+    return indicator
+
+print(indicationADX('MSFT').adx_pos())
